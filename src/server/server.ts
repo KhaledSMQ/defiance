a8cf4a200f0bbf4d57981212b55c46a681b5dd36
Tyler Coles <shidobu@gmail.com>
2016-11-22 21:04:28 -0500

    Updated project to support/fix VS Code debugging.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/index.d.ts" />
+/// <reference path="./typings/index.d.ts" />
 
 import express = require('express');
 import BaseRoutes = require("./config/routes/Routes");
