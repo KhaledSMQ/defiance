daebc8734d1a77cb404d05beeabb134f6b8b90bf
Tyler Coles <shidobu@gmail.com>
2016-12-04 23:55:53 -0500

    Added some generic socket constants

--- src/shared/constants/SocketEventNames.ts
+++ src/shared/constants/SocketEventNames.ts
@@ -2,4 +2,19 @@
 
 export class SocketEventNames {
 
+    private static _client: { [name: string]: string } = {
+        sendDataToServer: "dataToServer"
+    }
+
+    private static _server: { [name: string]: string } = {
+        sendDataToClient: "dataToClient"
+    }
+
+    static get client(): { [name: string]: string } {
+        return SocketEventNames._client;
+    }
+
+    static get server(): { [name: string]: string } {
+        return SocketEventNames._server;
+    }
 }
